a8e4c49dfed1d11c569e5ae2bf48768ad96d1e5c
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-11-06 20:19:53 +0900

    describe prototype complete

---
+++ describe.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+# import statistic
+
+
+def describe(df):
+    """
+    describe main statistic
+    
+    mean, std, max, min
+
+    Args:
+        df (pd.DataFrame): pandas DataFrame datatype
+    """
+    numeric_list = []
+    for col in df:
+        if df[col].dtype == 'float64' or df[col].dtype == 'int64':
+            numeric_list.append(col)
+    statistic_df = pd.DataFrame(columns=numeric_list)
+    count = {}
+    for col in numeric_list:
+        count[col] = df.count()[col]
+    statistic_df.loc['count'] = count
+    mean = {}
+    for col in numeric_list:
+        mean[col] = np.mean(df[col])
+    statistic_df.loc['mean'] = mean
+    std = {}
+    for col in numeric_list:
+        std[col] = np.std(df[col])
+    statistic_df.loc['std'] = std
+    min = {}
+    for col in numeric_list:
+        min[col] = np.min(df[col])
+    statistic_df.loc['min'] = min
+    q25 = {}
+    for col in numeric_list:
+        q25[col] = np.nanpercentile(df[col], q=25)
+    statistic_df.loc['25%'] = q25
+    q50 = {}
+    for col in numeric_list:
+        q50[col] = np.nanpercentile(df[col], q=50)
+    statistic_df.loc['50%'] = q50
+    q75 = {}
+    for col in numeric_list:
+        q75[col] = np.nanpercentile(df[col], q=75)
+    statistic_df.loc['75%'] = q75
+    max = {}
+    for col in numeric_list:
+        max[col] = np.max(df[col])
+    statistic_df.loc['max'] = max
+    # print(mean)
+    return statistic_df
+    
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+import pandas as pd
+
+from describe import describe
+
 def main():
-    pass
+    try:
+        df = pd.read_csv(sys.argv[1])
+    except Exception as e:
+        print(f"{e}")
+    print(df.describe())
+    print(describe(df))
+    
 
 if __name__ == "__main__":
     main()
